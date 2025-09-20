aea4fbce025ad8e64481155e222942b9af4c8493
Micah Bjorkgren <bjorkgmd@dukes.jmu.edu>
2025-09-20 17:47:59 -0400

    Debugged and added error messages

--- Parsing.rb
+++ Parsing.rb
@@ -8,7 +8,7 @@ class Parser
     end
 
     def parse
-        level6
+        level0
     end
 
     def advance
@@ -35,23 +35,34 @@ class Parser
     end
 
     def level1
-        if has(:print)
-            advance
-            right = level2
-            left = Print.new(right)
-        else
-            left = level2
+        left = level2
+        i = false
+        while true
+            if has(:print)
+                raise "More then one print at index #{tokens[@i].start}" if i
+                advance
+                right = level2
+                left = Print.new(right)
+                i = true
+            else
+                break
+            end
         end
         left 
     end
 
-    def level 2
-        if has(:var)
-            left = level3
+    def level2
+        left = level3
+        i = false
+        while true
             if has(:assign)
+                raise "More then one assign at index #{tokens[@i].start}" if i
                 advance
                 right = level3
                 left = Assignment.new(left, right)
+                i = true
+            else
+                break
             end
         end
         left
@@ -61,12 +72,16 @@ class Parser
         left = level4
         while true
             if has(:and)
+                raise "Missing operand left of logical and at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
                 advance
                 right = level4
+                raise "Missing operand right of logical and at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = AndLogicalOperation.new(left, right)
             elsif has(:or)
+                raise "Missing operand left of logical or at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
                 advance
                 right = level4
+                raise "Missing operand right of logical or at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = OrLogicalOperation.new(left, right)
             else
                 break
@@ -79,28 +94,40 @@ class Parser
         left = level5
         while true
             if has(:equal)
-                advance 
+                raise "Missing operand left of equal at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
+                advance
                 right = level5
+                raise "Missing operand right of equal at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = EqualsOperation.new(left, right)
             elsif has(:nequal)
-                advance 
+                raise "Missing operand left of not equal at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
+                advance
                 right = level5
+                raise "Missing operand right of not equal at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = NotEqualsOperation.new(left, right)
             elsif has(:greater)
-                advance 
+                raise "Missing operand left of greater than at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
+                advance
                 right = level5
+                raise "Missing operand right of greater than at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = GreaterThanOperation.new(left, right)
             elsif has(:greaterequal)
-                advance 
+                raise "Missing operand left of greater or equal than at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
+                advance
                 right = level5
+                raise "Missing operand right of greater or equal than at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = GreaterThanOrEqualOperation.new(left, right)
             elsif has(:less)
-                advance 
+                raise "Missing operand left of less than at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
+                advance
                 right = level5
+                raise "Missing operand right of less than at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = LessThanOperation.new(left, right)
             elsif has(:lessequal)
-                advance 
+                raise "Missing operand left of less or equal than at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
+                advance
                 right = level5
+                raise "Missing operand right of less or equal than at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = LessThanOrEqualOperation.new(left, right)
             else
                 break
@@ -113,24 +140,34 @@ class Parser
         left = level6
         while true
             if has(:band)
+                raise "Missing operand left of bit and at index #{tokens[@i].start}" if  left == nil
                 advance
                 right = level6
+                raise "Missing operand right of bit and at index #{tokens[@i - 1].start}" if  right == nil
                 left = BitAndOperation.new(left, right)
             elsif has(:bor)
+                raise "Missing operand left of bit or at index #{tokens[@i].start}" if  left == nil
                 advance
                 right = level6
+                raise "Missing operand right of bit or at index #{tokens[@i - 1].start}" if  right == nil
                 left = BitOrOperation.new(left, right)
             elsif has(:xor)
+                raise "Missing operand left of bit xor at index #{tokens[@i].start}" if  left == nil
                 advance
                 right = level6
+                raise "Missing operand right of bit xor at index #{tokens[@i - 1].start}" if  right == nil
                 left = BitXorOperation.new(left, right)
             elsif has(:left)
+                raise "Missing operand left of left shift at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
                 advance
                 right = level6
+                raise "Missing operand right of left shift at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = LeftShiftOperation.new(left, right)
             elsif has(:right)
+                raise "Missing operand left of right shift at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
                 advance
                 right = level6
+                raise "Missing operand right of right shift at index #{tokens[@i - 1].start} to #{tokens[@i -1].back}" if  right == nil
                 left = RightShiftOperation.new(left, right) 
             else
                 break
@@ -143,12 +180,16 @@ class Parser
         left = level7
         while true
             if has(:plus)
+                raise "Missing operand left of addition at index #{tokens[@i].start}" if  left == nil
                 advance
                 right = level7
+                raise "Missing operand right of addition at index #{tokens[@i - 1].start}" if  right == nil
                 left = AddArithmeticOperation.new(left, right)
             elsif has(:minus)
+                raise "Missing operand left of subtraction at index #{tokens[@i].start}" if  left == nil
                 advance
                 right = level7
+                raise "Missing operand right of subtraction at index #{tokens[@i - 1].start}" if  right == nil
                 left = SubtractArithmeticOperation.new(left, right)
             else
                 break
@@ -161,16 +202,22 @@ class Parser
         left = level8
         while true
             if has(:multiply)
+                raise "Missing operand left of multiplication at index #{tokens[@i].start}" if  left == nil
                 advance
                 right = level8
+                raise "Missing operand right of multiplication at index #{tokens[@i - 1].start}" if  right == nil
                 left = MultiplyArithmeticOperation.new(left, right)
             elsif has(:divide)
+                raise "Missing operand left of division at index #{tokens[@i].start}" if  left == nil
                 advance
                 right = level8
+                raise "Missing operand right of division at index #{tokens[@i - 1].start}" if  right == nil
                 left = DivideArithmeticOperation.new(left, right)
             elsif has(:modulo)
+                raise "Missing operand left of modulo at index #{tokens[@i].start}" if  left == nil
                 advance
                 right = level8
+                raise "Missing operand right of modulo at index #{tokens[@i - 1].start}" if  right == nil
                 left = ModuloArithmeticOperation.new(left, right)
             else
                 break
@@ -183,8 +230,10 @@ class Parser
         left = level9
         while true
             if has(:exponent)
+                raise "Missing operand left of exponenet at index #{tokens[@i].start} to #{tokens[@i].back}" if  left == nil
                 advance
                 right = level8
+                raise "Missing operand right of exponenet at index #{tokens[@i - 1].start} to #{tokens[@i - 1].back}" if  right == nil
                 left = ExponentArithmeticOperation.new(left, right)
             else
                 break
@@ -198,11 +247,13 @@ class Parser
         while true
             if has(:float_cast)
                 advance
-                right = level10
+                right = level9
+                raise "Missing operand right of float cast at index #{tokens[@i - 1].start}" if  right == nil
                 left = IntToFloat.new(right)
             elsif has(:int_cast)    
                 advance
-                right = level10
+                right = level9
+                raise "Missing operand right of int cast at index #{tokens[@i - 1].start}" if  right == nil
                 left = FloatToInt.new(right)
             else
                 break
@@ -216,15 +267,18 @@ class Parser
         while true 
             if has(:not)
                 advance
-                right = level11
+                right = level10
+                raise "Missing operand right of not at index #{tokens[@i - 1].start}" if  right == nil
                 left = NotLogicalOperation.new(right)
             elsif has(:bnot)
                 advance
-                right = level11
+                right = level10
+                raise "Missing operand right of bit not at index #{tokens[@i - 1].start}" if  right == nil
                 left = BitNotOperation.new(right)
             elsif has(:negation)
                 advance
-                right = level11
+                right = level10
+                raise "Missing operand right of negation at index #{tokens[@i - 1].start}" if  right == nil
                 left = NegationArithmeticOperation.new(right)
             else
                 break
@@ -234,26 +288,28 @@ class Parser
     end
 
     def level11
-        if has(:int)
+        if has(:var)
+            value = Variable.new(advance.text)
+        elsif has(:int)
             value = IntegerPrimitive.new(advance.text.to_i)
         elsif has(:float)
             value = FloatPrimitive.new(advance.text.to_f)
         elsif has(:string)
             value = StringPrimitive.new(advance.text)
         elsif has(:true)
+            advance
             value = BooleanPrimitive.new(1)
         elsif has(:false)
+            advance
             value = BooleanPrimitive.new(nil)
         elsif has(:null)
+            advance
             value = NullPrimitive.new
         elsif has(:leftbracket)
             advance
-            value = level5
-            if !has(:rightbracket)
-                raise "Unclosed parathensis at #{@i}"
-            end
-        else
-            raise "Unknown primitive #{@tokens[@i - 1].text} at #{@i}"
+            value = level3
+            raise "Unclosed parathensis at #{@i}" if !has(:rightbracket)
+            advance
         end
         value
     end
